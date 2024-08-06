698a994e7bd219d54f1f477ebfc7d8b3e84a1630
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-08-07 00:04:10 +0530

    url

--- frontend/src/components/UrlPreview.tsx
+++ frontend/src/components/UrlPreview.tsx
@@ -3,15 +3,15 @@ import { InputField } from "./InputField";
 export function UrlPreview({generatedUrl} : {generatedUrl : string}){
     return(
         generatedUrl && (
-            <div className='w-2/3 h-[20vh] p-2 flex flex-col justify-between items-center border-2 border-black bg-neutral-900 text-white rounded'>
+            <div className='w-2/3 h-[15vh] p-2 flex flex-col justify-between items-center border-2 border-black bg-neutral-900 text-white rounded'>
                 <div className='w-full flex justify-between items-center'>
                     <span className="font-semibold">Generated URL:</span>
-                    <button
+                    {/* <button
                         onClick={() => navigator.clipboard.writeText(generatedUrl)}
                         className="px-2 py-1 border-2 border-black bg-neutral-800 rounded-lg text-blue-500 hover:bg-black hover:text-white transition-colors duration-100"
                     >
                         Copy
-                    </button>
+                    </button> */}
                 </div>
                 <InputField
                     onInputChange={()=>{}}
